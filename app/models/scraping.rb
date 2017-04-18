873ee8b61f5f9bc41fb81c021c1329e599af226a
George Sekiya <joji.sekiya@gmail.com>
2017-04-18 17:16:37 +0900

    Update scraping file

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -2,38 +2,39 @@ class Scraping
   def self.movie_urls
     agent = Mechanize.new
     links = []
-    next_url= "/now/"
+    next_url = ""
 
     while true
-      current_page = agent.get("http://eiga.com" + next_url)
-      elements = current_page.search('.m_unit h3 a')
+      current_page = agent.get("http://review-movie.herokuapp.com/" + next_url)
+      elements = current_page.search('.entry-title a')
       elements.each do |ele|
         links << ele.get_attribute('href')
       end
 
-      next_link = current_page.at('.next_page')
+      next_link = current_page.at('.pagination .next a')
+      break unless next_link
       next_url = next_link.get_attribute('href')
 
-      break unless next_url
     end
     links.each do |link|
-      get_product('http://eiga.com' + link)
+      get_product('http://review-movie.herokuapp.com' + link)
     end
   end
 
   def self.get_product(link)
-    agent = Matsushitaze.new
+    agent = Mechanize.new
     page = agent.get(link)
-    title = page.at('.moveInfoBox h1').inner_text
-    image_url = page.at('.pictBox img')[:src] if page.at('.pictBox img')
-    director = page.at('.f span').inner_text if page.at('.f span')
-    detail = page.at('.outline p').inner_text
-    open_date = page.at('.opn_date strong').inner_text if page.at('.opn_date strong')
+    title = page.at('.entry-title').inner_text if page.at('.entry-title')
 
-    product = Product.where(title: title, image_url: image_url).first_or_initialize
+    image_url = page.at('.entry-content img')[:src] if page.at('.entry-content img')
+    director = page.at('.director span').inner_text if page.at('.director span')
+    detail = page.at('.entry-content p').inner_text if page.at('.entry-content p')
+    open_date = page.at('.date span').inner_text if page.at('.date span')
+
+    product = Product.where(title: title).first_or_initialize
+    product.image_url = image_url
     product.director = director
-    product.detail = detail
+    product.detail = detial
     product.open_date = open_date
-    product.save
   end
 end
